10d2b77a6227b9eaa7098bc2a77bf2b902ef2564
Christian L. Ouellette <clo006@bucknell.edu>
2017-11-14 13:35:46 -0500

    updates test8

--- test8.js
+++ test8.js
@@ -1,3 +1,18 @@
+/require the express nodejs module
+var express = require('express'),
+    //set an instance of exress
+    app = express(),
+    //require the body-parser nodejs module
+    bodyParser = require('body-parser'),
+    //require the path nodejs module
+    path = require("path");
+	
+	//support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //tell express that www is the root of our public web folder
 app.use(express.static(path.join(__dirname, 'www')));
 
