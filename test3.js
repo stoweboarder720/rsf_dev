0c5f37e867033d8c69bd1b1d85646ac9af5fe861
stoweboarder720 <stoweboarder720@gmail.com>
2017-11-15 15:58:30 -0500

    newtable

--- test3.js
+++ test3.js
@@ -10,7 +10,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  var sql = "CREATE TABLE rsftest (satisfaction INT, appIssue VARCHAR(512), pathIssue VARCHAR(512), likes VARCHAR(256), improve VARCHAR(256))";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
